796038055b3fe26b78868c115ac1ac1cbf6b3f77
Siim Puidet <siim.puidet@tktk.ee>
2024-11-28 13:52:18 +0200

    add week5 code

---
+++ OROperator/Program.cs
@@ -0,0 +1,20 @@
+﻿//Math >= 90 && Chemistry >= 90 || Math >= 90 && Biology >= 90 || Chemistry >= 90 && Biology >= 90
+int math, biology, chemistry; //sisestame muutujad vahemällu
+
+Console.WriteLine("Enter your Math result:"); //palume kasutajal matemaatika eksami tulemus sisestada
+math = Int32.Parse(Console.ReadLine()); //muudame selle arvväärtuseks
+
+Console.WriteLine("Enter your Biology result:"); //palume kasutajal bioloogia eksami tulemus sisestada
+biology = Int32.Parse(Console.ReadLine()); //muudame selle arvväärtuseks
+
+Console.WriteLine("Enter your Chemistry result:"); //palume kasutajal keemia eksami tulemus sisestada
+chemistry = Int32.Parse(Console.ReadLine()); //muudame selle arvväärtuseks
+
+if((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >= 90)) //seame tingimuse, et vähemalt kaks eksami tulemust peavad olema >= 90%
+{
+    Console.WriteLine("Congratulations! You got accepted."); //kui tingimus on täidetud, õnnitleme
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved."); //kui tingimus ei ole täidetud, teatame ebaõnnnestumisest
+}
---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,17 @@
+﻿//AND - &&
+
+// Kõik tingimused peavad olema täidetud ehk kõigi väljund true.
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+
+//OR - || (pipes)
+
+// Vähemalt üks tingimusetest peab olema täidetud ehk vähemalt ühe väljund true.
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
+
